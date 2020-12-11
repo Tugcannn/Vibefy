aba90a994035d634965ab1e1f16650e38e0d4f0b
Uygar KAYA <uygar.kaya@ozu.edu.tr>
2020-12-11 15:23:03 +0300

    Header file created and index file edited.

---
+++ Header.js
@@ -0,0 +1,10 @@
+document.getElementById("Header").innerHTML = "<div class='header'>" +
+"<div class='header-left'>" +
+    "<i class='logox'>Vibefy</i>" +
+"</div>" +
+"<div class='header-right'> " +
+    "<a class='active' href='#home'><i>Home</i></a>" +
+    "<a href='#contact'><i>Contact</i></a>" +
+    "<a href='#about'><i>About</i></a>" +
+"</div>" +
+"</div>";
